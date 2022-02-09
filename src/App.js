643b8d8be8b98a2a9da1f07826bf4fd18a762dfc
Alex <olivesargreek@gmail.com>
2022-02-09 10:31:58 +0300

    add: getCountries function && bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [ countries, setCountries ] = useState([]);
-  const [loading, setLoading ] = useState(false);
+
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [countriesPerPage, setcountriesPerPage] = useState(10);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      setLoading(true);
+      const res = await axios.get(' https://restcountries.eu/rest/v2/all');
+      setCountries(res.data);
+      setLoading(false);
+    }
+    getCountries();
+  }, []);
+
   return (
-    <div className="App">
+    <div className="container mt-5">
+      <h1 className="text-primary">Countries</h1>
       
     </div>
   );
